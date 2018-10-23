b95f3642f478b9d5875b6ddf2d4246b03389c13f
sunyikang <sunyikang@gmail.com>
2018-10-23 17:05:46 +0800

    homework instruction

---
+++ 04_homework_fruit_processor.rb
@@ -0,0 +1,9 @@
+# create a fruit processor to process Apple and Banana, requirement is below:
+# 1. they all have a property like color, weight
+# 2. they all have method Process() and output is AppleJuice and BananaJuice and BlendJuice
+# 3. they need to be initialized in the very beginning
+# 4. there is a input for different fruits, but need to output a juice with 
+# 4.1 different weight (sum of existing fruit)
+# 4.2 different color (red, yellow or blending)
+
+
